50dfb5d880bc40d05b9298a73b559bec76aea1b8
GhrollorhG <39232926+Iconsana@users.noreply.github.com>
2024-12-24 12:38:01 +0200

    Update app.py

--- app.py
+++ app.py
@@ -30,17 +30,15 @@ handler.setFormatter(logging.Formatter(
 logger.addHandler(handler)
 
 class ACDCStockScraper:
-    def __init__(self):
-        options = Options()
-        options.add_argument('--headless')
-        options.add_argument('--no-sandbox')
-        options.add_argument('--disable-dev-shm-usage')
-        options.add_argument('--disable-gpu')
-        options.add_argument('--remote-debugging-port=9222')
-        service = webdriver.firefox.service.Service()
-        service.start()
-        self.driver = webdriver.Firefox(options=options, service=service)
-        self.wait = WebDriverWait(self.driver, 10)
+   def __init__(self):
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--disable-gpu')
+    options.binary_location = '/usr/bin/firefox'
+    self.driver = webdriver.Firefox(options=options)
+    self.wait = WebDriverWait(self.driver, 10)
 
     def get_stock_levels(self, sku):
         try:
